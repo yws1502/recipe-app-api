12a5caf78c7b6a51f07aa017ede55b69564a2478
Woosang <yws1502@naver.com>
2021-10-26 16:57:53 +0900

    Create list recipe feature.

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredents', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
